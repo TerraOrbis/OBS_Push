13f6217e622e74d845a003b801eb8f7e91c45c29
dvkirn <dvkirn@users.noreply.github.com>
2023-08-05 11:49:28 -0400

    Update obs_push.lua

--- obs_push.lua
+++ obs_push.lua
@@ -1,20 +1,14 @@
--- OBS_Push a vlc plugin - public domain
--- Written by dvkirn in 2015
+-- OBS_Push a vlc plugin
 --
--- A simple vlc plugin to push the current playing track info to a file
+-- pushes the current playing track info to a file
 -- OBS will read from the file updating the stream overlay
 --
--- Placed in public domain March 2015: no copyright claimed
--- do with this what you will.
---
--- Installation: simply put in extensions folder
+-- Installation: extensions folder
 --     Windows: C:\Program Files (x86)\VideoLAN\VLC\lua\extensions
 --     Linux: ~/.local/share/vlc/lua/extensions
 --     OS X: VLC.app/Mac OS/share/lua/extensions (I think)
 --
--- Usage: OBS_Push should be under the View menu. Simply click to activate.
--- Also it seems there needs to be a track playing before it will startup.
--- I don't know exactly why this is but it works fine after that.
+-- OBS_Push should be under the View menu
 --
 
 --output file path--
